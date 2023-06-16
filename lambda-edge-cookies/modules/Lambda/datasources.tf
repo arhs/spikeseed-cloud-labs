b6274d59fd269714a3d0b8549d8465872b0f220e
Strahil Hristov <Strahil.Hristov@arhs-spikeseed.com>
2023-06-16 10:13:15 +0300

    delete lambda-edge-code

--- lambda-edge-cookies/modules/Lambda/datasources.tf
+++
@@ -1,5 +0,0 @@
-data "archive_file" "lambda_edge_cookie" {
-  type        = "zip"
-  source_dir  = "${path.module}/template"
-  output_path = "${path.module}/lambda_edge_cookie.zip"
-}
--- lambda-edge-cookies/modules/Lambda/output.tf
+++
@@ -1,8 +0,0 @@
-output "lambda_function_arn" {
-  value = aws_lambda_function.main.arn
-}
-
-output "lambda_function_version" {
-  value = aws_lambda_function.main.version
-}
-
